1e910020cc56a542c67f7edc9c58a13f50efa82a
mego018 <megobus36@gmail.com>
2025-09-13 19:27:21 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,124 +1,107 @@
-import kivy
-kivy.require('1.9.1')
 from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.scatter import Scatter
+from kivy.uix.image import Image
 from kivy.uix.button import Button
-from kivy.uix.relativelayout import RelativeLayout
-from kivy.uix.textinput import TextInput
-from kivy.animation import Animation
+from kivy.uix.slider import Slider
+from kivy.uix.filechooser import FileChooserIconView
+from kivy.uix.popup import Popup
 
-Builder.load_string('''
-<updlbl>:
-    size:500,1000
-    canvas.before: 
-        Color: 
-            rgba: (1, 1, 1, 1) 
-        Rectangle: 
-            source:'background.png'
-            size: root.width, root.height 
-            pos: self.pos
-    Label:
-        id: updlbl
-        text: '[b]Output[/b]:'
-		font_size:"25sp"
-		pos_hint:{"x":-0.3,"y":-0.03}
-		color: "5d18d6"
-		markup: True
-	Label:
-    	text:"Rgb/Hex converter"
-    	pos_hint:{"y":0.46}
-    	background_color: "3d3d3d"
-    	font_size:"25sp"
-	Label:
-		text:"this is a small app by yashas :-)"
-		font_size:"8sp"
-	    color:(1,1,1,1)
- 	   pos_hint:{"x":0,"y":-0.4}
-	TextInput: 
-        id: input
-        hint_text:'Enter hex/rgb code:'
-        pos_hint: {'x': 0.1, 'y': 0.8} 
-        font_size: "20sp"
-        size_hint: 0.8, 0.09
-        padding: 40
-    Label:
-    	text:"hex format: xxxxxx   and   rgb format: xxx,xxx,xxx"
-    	font_size:"10sp"
-    	pos_hint:{"y":.27}
-    Button:
-        text: 'Convert'
-		size_hint:.4,.08
-		pos_hint:{"x":0.28,"y":0.6}
-		border:1,1,1,1
-		on_press:root.convert()
-	TextInput: 
-        id: output
-        hint_text:'Output will be shown here'
-        pos_hint: {'x': 0.1, 'y': 0.3}
-        font_size: "20sp"
-        size_hint: 0.8, 0.09
-        padding: 40
- ''')
+# For WebView on Android
+from kivy.utils import platform
+if platform == 'android':
+    from jnius import autoclass
+    from android.runnable import run_on_ui_thread
 
-class updlbl(RelativeLayout):
-     def __init__(self, **kwargs):
-       super(updlbl,self).__init__(**kwargs)
-       pass
-     def ch_text(self, txxt):
-     	       self.ids.updlbl.text=txxt
-     	       anim = Animation(x=50, size=(80, 80), t='in_quad')
-     	       anim += Animation(x=-50,size=(40, 40), t='in_quad')
-     	       anim.repeat=True
-     	       anim.start(self.ids.updlbl)
-     def toHEX(self,sett):
-     		try:
-     			val = sett.split(",")
-     			conv = {0:0,1:1,2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9,10:"a",11:"b",12:"c",13:"d",14:"e",15:"f",16:"g"}
-     			t1 = str(float(val[0])/16).split(".")
-     			v1 = conv[int(t1[0])]
-     			v2 = conv[int(float("0."+t1[1])*16)]
-     			t2 = str(float(val[1])/16).split(".")
-     			v3 = conv[int(t2[0])]
-     			v4 = conv[int(float("0."+t2[1])*16)]
-     			t3 = str(float(val[2])/16).split(".")
-     			v5 = conv[int(t3[0])]
-     			v6 = conv[int(float("0."+t3[1])*16)]
-     			self.ids.output.text = f" #{v1}{v2}{v3}{v4}{v5}{v6}"
-     		except:
-     			self.ids.output.text = "Something is not correct, \ncheck again example of rgb code: 200,20,60"
-     def toRGB(self,sett):
-     		conv = {0:0,1:1,2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9,10:"a",11:"b",12:"c",13:"d",14:"e",15:"f",16:"g"}
-     		try:
-     			conv2 = {str(v): k for k, v in conv.items()}
-     			h1 = []
-     			for bb in sett:
-     				h1.append(bb)
-     			n1 = (int(conv2[h1[0]])*16)+(int(conv2[h1[1]]))
-     			n2 = (int(conv2[h1[2]])*16)+(int(conv2[h1[3]]))
-     			n3 = (int(conv2[h1[4]])*16)+(int(conv2[h1[5]]))
-     			hk = f"rgb({n1},{n2},{n3})"
-     			self.ids.output.text = hk
-     		except:
-     			self.ids.output.text = "something is not correct, \nplease enter in small letters and try. error:HEX"
-     			
-     def convert(self):
-     	code = "HEX"
-     	sett = self.ids.input.text
-     	for x in sett:
-     		if x == ',':
-     			code = "RGB"
-     		if code == "RGB":
-     			self.toHEX(sett)
-     		elif code == "HEX":
-     			self.toRGB(sett)
-     		else:
-     			self.ids.output.text = "oh no! something went wrong"
-	  
-class UpdateLabel(App):
-     def build(self):
-     	self.title = "Update Label"
-     	return updlbl()
+class MagmaApp(App):
+    def build(self):
+        root = FloatLayout()
 
-if __name__ == '__main__':
-    UpdateLabel().run()
+        # === Scatter for the floating image ===
+        self.scatter = Scatter(size_hint=(None, None), size=(300, 300))
+        self.img = Image(size=self.scatter.size, allow_stretch=True)
+        self.scatter.add_widget(self.img)
+        root.add_widget(self.scatter)
+
+        # === Transparency slider ===
+        self.opacity_slider = Slider(min=0.1, max=1, value=1, size_hint=(0.3, None), height=50, pos=(50, 10))
+        self.opacity_slider.bind(value=self.change_opacity)
+        root.add_widget(self.opacity_slider)
+
+        # === Lock button ===
+        self.locked = False
+        lock_btn = Button(text="Lock", size_hint=(0.2, None), height=50, pos=(400, 10))
+        lock_btn.bind(on_press=self.toggle_lock)
+        root.add_widget(lock_btn)
+
+        # === Add image button ===
+        add_img_btn = Button(text="Add Image", size_hint=(0.2, None), height=50, pos=(50, 70))
+        add_img_btn.bind(on_press=self.open_filechooser)
+        root.add_widget(add_img_btn)
+
+        # === Zoom in button ===
+        zoom_in_btn = Button(text="Zoom In", size_hint=(0.2, None), height=50, pos=(400, 70))
+        zoom_in_btn.bind(on_press=lambda x: self.zoom_image(1.2))
+        root.add_widget(zoom_in_btn)
+
+        # === Zoom out button ===
+        zoom_out_btn = Button(text="Zoom Out", size_hint=(0.2, None), height=50, pos=(400, 130))
+        zoom_out_btn.bind(on_press=lambda x: self.zoom_image(0.8))
+        root.add_widget(zoom_out_btn)
+
+        # === WebView for Android ===
+        if platform == 'android':
+            self.add_webview(root)
+
+        return root
+
+    # === Image functions ===
+    def change_opacity(self, instance, value):
+        self.img.opacity = value
+
+    def toggle_lock(self, instance):
+        self.locked = not self.locked
+        self.scatter.do_translation = not self.locked
+        self.scatter.do_scale = not self.locked
+        self.scatter.do_rotation = not self.locked
+        instance.text = "Lock" if not self.locked else "Unlock"
+
+    def open_filechooser(self, instance):
+        content = FileChooserIconView()
+        popup = Popup(title="Choose an image", content=content, size_hint=(0.9, 0.9))
+        
+        def load_selection(*args):
+            if content.selection:
+                self.img.source = content.selection[0]
+                self.img.reload()
+                popup.dismiss()
+        
+        content.bind(on_submit=lambda *a: load_selection())
+        popup.open()
+
+    def zoom_image(self, factor):
+        # Zoom in or out depending on factor
+        new_width = self.scatter.width * factor
+        new_height = self.scatter.height * factor
+        self.scatter.size = (new_width, new_height)
+        self.img.size = self.scatter.size
+
+    # === WebView on Android ===
+    if platform == 'android':
+        @run_on_ui_thread
+        def add_webview(self, root):
+            PythonActivity = autoclass('org.kivy.android.PythonActivity')
+            WebView = autoclass('android.webkit.WebView')
+            WebViewClient = autoclass('android.webkit.WebViewClient')
+            
+            activity = PythonActivity.mActivity
+            self.webview = WebView(activity)
+            self.webview.getSettings().setJavaScriptEnabled(True)
+            self.webview.setWebViewClient(WebViewClient())
+            self.webview.loadUrl("https://magma.com/home")
+            
+            layout = activity.findViewById(0x01020002)  # android.R.id.content
+            layout.addView(self.webview)
+
+if __name__ == "__main__":
+    MagmaApp().run()
